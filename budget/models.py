4adfead37c8de141e5a8e23c98458abe8f0043c4
Serp <manu.serpette74@gmail.com>
2022-11-02 17:03:35 -0400

    patch

--- budget/models.py
+++ budget/models.py
@@ -25,23 +25,21 @@ class Project(models.Model):
         expense_amount = total_temp
         new_budget_temp = budget_temp - expense_amount
 
-
         for expense in expense_list:
-            total_expense_amount += expense_amount
+            total_expense_amount += expense.amount
 
         # temporary solution, because the form currently only allows integer amounts
         total_expense_amount = int(total_expense_amount)
         return self.budget - total_expense_amount
-
 
     @property
     def total_transactions(self):
         expense_list = Expense.objects.filter(project=self)
         return len(expense_list)
 
-
     def get_absolute_url(self):
         return '/' + self.slug
+
 
 class Category(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
@@ -49,7 +47,8 @@ class Category(models.Model):
 
 
 class Expense(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='expenses')
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name='expenses')
     title = models.CharField(max_length=125)
     amount = models.DecimalField(max_digits=12, decimal_places=3)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
